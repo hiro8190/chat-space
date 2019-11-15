d0cd388e444f918a272d06121a7e24bbe0cfd804
hiro8190 <kdr.8190@gmail.com>
2019-11-15 16:11:07 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,25 +1,11 @@
 $(function(){
 
-  function buildMessage_withimg(message){
-  
-    var html = `<div class="upper-message">
-                  <div class="upper-message__user-name">
-                    ${message.name}
-                  </div>
-                  <div class="upper-message__date">
-                    ${message.created_at}
-                  </div>
-                </div>
-                <div class="lower-message">
-                  <p class="lower-message__content">
-                    ${message.content}
-                  </p>
-                  <img class="lower-message__image" src = '${message.image.url}'></img>
-                </div>`
-    return html;
-  }
-
   function buildMessage(message){
+    if (message.image.url !== null){
+      var image = message.image.url;
+    }else{
+      var image = '';
+    }
   
     var html = `<div class="upper-message">
                   <div class="upper-message__user-name">
@@ -33,6 +19,7 @@ $(function(){
                   <p class="lower-message__content">
                     ${message.content}
                   </p>
+                  <img class="lower-message__image" src = '${image}'></img>
                 </div>`
     return html;
   }
@@ -50,12 +37,7 @@ $(function(){
       contentType: false
     })
     .done(function(message){
-      console.log(message)
-      if (message.image.url !== null){
-        var html = buildMessage_withimg(message);
-      }else{
-        var html = buildMessage(message);
-      }
+      var html = buildMessage(message);
       $('.messages').append(html);
       $('.new_message')[0].reset();
       $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
