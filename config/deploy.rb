190a15c8adb661643313a073436112e7f094886d
hiro8190 <kdr.8190@gmail.com>
2019-11-19 19:11:33 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,10 +56,3 @@ namespace :deploy do
   after :finishing, 'deploy:cleanup'
 end
 
-# デプロイ処理が終わった後、Unicornを再起動するための記述
-after 'deploy:publishing', 'deploy:restart'
-namespace :deploy do
-  task :restart do
-    invoke 'unicorn:restart'
-  end
-end
